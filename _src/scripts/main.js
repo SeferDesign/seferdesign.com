2aca90b6ad73c1eaf050e693b7b1b6de959a3ece
Robert Sefer <rsefer@gmail.com>
2015-12-04 23:01:11 -0600

    Major work

--- _src/scripts/main.js
+++ _src/scripts/main.js
@@ -1,11 +1,2 @@
 jQuery(document).ready(function($) {
-
-  $('a.scroll').click(function(e) {
-    e.preventDefault();
-
-    $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top - 20
-    }, 500);
-  });
-
 });
