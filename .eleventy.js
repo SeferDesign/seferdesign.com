4f84ea8dfb09c47ddab3769a32ff74a3a4a1a4de
Robert Sefer <rsefer@gmail.com>
2023-05-16 12:29:27 -0500

    Removed webp

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const Image = require('@11ty/eleventy-img');
 
 async function imageShortcode(src, alt, klass = '', loading = 'lazy', sizes = null) {
 	let extension = src.split('.').pop();
-	let formats = ['webp', 'jpeg'];
+	let formats = ['jpeg']; // webp, avif
 	if (extension == 'svg') {
 		formats = ['svg'];
 	}
