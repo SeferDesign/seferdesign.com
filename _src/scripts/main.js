ca6f6b56330d250cb42d94403940a7a8efd17cbf
Robert Sefer <rsefer@gmail.com>
2026-05-01 16:17:16 -0500

    Nav + main height fixes

--- _src/scripts/main.js
+++ _src/scripts/main.js
@@ -1,14 +1,15 @@
-function setVH() {
-	document.documentElement.style.setProperty('--vh', (Math.ceil((window.innerHeight * 0.01) * 100) / 100) + 'px');
+function setSizingVariables() {
+	let navHeight = document.querySelector('nav#main-nav') ? document.querySelector('nav#main-nav').offsetHeight : 0;
+	document.documentElement.style.setProperty('--nav-height', navHeight + 'px');
 }
 
-setVH();
+setSizingVariables();
 
 document.getElementById('nav-main-toggle').addEventListener('click', function(event) {
   document.body.classList.toggle('nav-expanded');
 });
 
 window.addEventListener('resize', function() {
-	setVH();
+	setSizingVariables();
 	document.body.classList.remove('nav-expanded');
 });
